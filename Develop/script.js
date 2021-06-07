80bfb72b3337d2d3b01ce6f4c86cddca12f99108
David Szamek <davidszamek@gmail.com>
2021-06-07 23:00:05 +1000

    Debugging code: attempting to get password to show in box

--- Develop/script.js
+++ Develop/script.js
@@ -23,56 +23,67 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numbers = "0123456789"
 var symbols = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-
 // CREATE THE FUNCTION
 
 function generatePassword() {
 
   var password = "";
   var passwordLength = "";
+  var passwordChoice = "";
 
   // User generates the desired length of password
 
-  var length = prompt("Enter a number from 8 to 128 for password length");
-  length = parseInt(length);
+  var passwordLength = prompt("Enter a number from 8 to 128 for password length");
+  passwordLength = parseInt(passwordLength);
 
-  if (length < 8 || length > 128) {
+  // If user doesn't a length between 8 & 128 ask them to try again. 
+
+  if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Your password length must be between 8 & 128 characters. Please try again.") 
     return ""
     }
+
+  console.log(passwordLength)
   
-  // creating variable "yes/no boolean comnfirmations"
+  // creating variable "yes/no" boolean confirmations which add the variables into a string if confirmed
 
   var lowerChoice = confirm("Would you like to throw in a few lowercase characters?");
 
   if (lowerChoice) {
-    passwordLength += lowerChoice; 
+    passwordChoice += lowerCase; 
   }
+
+console.log(lowerChoice)
 
   var upperChoice = confirm("How about some uppercase characters?");
 
   if (upperChoice) {
-    passwordLength += upperChoice;
+    passwordChoice += upperCase;
   } 
+
+  console.log(upperChoice)
 
   var numbersChoice = confirm("Spice things up with some numbers?");
 
   if (numbersChoice) {
-    passwordLength += numbersChoice; 
+    passwordChoice += numbers; 
   }
+
+  console.log(numbersChoice)
 
   var symbolChoice = confirm("Secure yourself with symbols?");
 
   if (symbolChoice) {
-    passwordLength += symbolChoice;
+    passwordChoice += symbols;
   }
 
-  for (var i = 0; i < length; i++) {
-    password = passwordLength [Math.floor(Math.random() * passwordLength.length)]
-  }
-}
+  console.log(symbolChoice)
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("password");
+  console.log(passwordChoice)
+
+  for (var i = 0; i < passwordLength; i++) {
+    password =+ passwordChoice[Math.floor(Math.random() * passwordChoice)];
+  }
+  
+  return password; 
 }
